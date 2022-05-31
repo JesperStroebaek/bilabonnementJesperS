3089257ea698ce3c42c72c9aa4c16f63f1066d2a
Jesper <jesp73g2@stud.kea.dk>
2022-05-31 20:53:37 +0200

    empty object??? jdbctemplate??

--- src/main/java/com/bilabonnementjespers/controllers/CarController.java
+++ src/main/java/com/bilabonnementjespers/controllers/CarController.java
@@ -4,10 +4,7 @@ import com.bilabonnementjespers.models.CarModel;
 import com.bilabonnementjespers.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,29 +12,36 @@ import java.util.List;
 public class CarController {
     @Autowired
     CarService carService;
-    @GetMapping("/cars-main")
-    public String carsMain() {
-        return "/cars-main";
-    }
+    @Autowired
+
+
     @GetMapping("/car-form")
     public String carForm() {
         return "/car-form";
     }
-    @PostMapping("/car-form")
-    public String carList(@ModelAttribute CarModel carsList){
-        carService.createCar(carsList);
-        return "redirect:/car-list";
-    }
-    @PostMapping("/submit-car-form")
-    public String createCar(Model model) {
-    List<CarModel> carModels = carService.carModelList();
-        model.addAttribute("cars", carModels);
-        return "redirect:/car-list";
-    }
-
-    @GetMapping("/car-list")
-    public String carList() {
+    /*
+    @GetMapping("submit-car-form")
+    public String carList2(Model model){
+    List<CarModel> carModels = carService.serviceList();
+        model.addAttribute("cars",carModels);
+        System.out.println(carModels);
         return "/car-list";
-        }
+    }*/
 
+    @PostMapping("/submit-car-form")
+    @ResponseBody
+    public String carForm(@RequestParam(value = "car_id") String car_id
+            , @RequestParam(value = "brand") String brand
+            , @RequestParam(value = "model") String model
+            , @RequestParam(value = "price") String price) {
+        System.out.println(car_id+brand+price+model);
+        return "/car-list";
+    }
+    @GetMapping("/car-list")
+    public void carList(){
+       List<CarModel> carModels = carService.serviceList();
+        System.out.println(carModels);
+        carService.createCar();
+
+    }
 }
