3f9bd8c89348c15a68a3120072fa690822f594c4
Jesper <jesp73g2@stud.kea.dk>
2023-01-29 06:37:09 +0100

    returntest started

--- src/main/java/com/bilabonnementjespers/models/ReturnTestModel.java
+++ src/main/java/com/bilabonnementjespers/models/ReturnTestModel.java
@@ -2,71 +2,74 @@ package com.bilabonnementjespers.models;
 
 public class ReturnTestModel {
 
-    int orderId;
-    int carId;
-    String firstName;
-    String lastName;
-    String repairDescription;
-    double repairPrice = -1;
+    int order_id;
+    int car_id;
+    String first_name;
+    String last_name;
+    String repair_description;
+    double repair_price = -1;
 
-    public ReturnTestModel(){
+    public ReturnTestModel(){};
 
-    };
-
-    public ReturnTestModel(int orderId, int carId, String firstName, String lastName, String repairDescription, double repairPrice) {
-        this.orderId = orderId;
-        this.carId = carId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.repairDescription = repairDescription;
-        this.repairPrice = repairPrice;
+    public ReturnTestModel(int order_id, int car_id, String first_name, String last_name, String repair_description, double repair_price) {
+        this.order_id = order_id;
+        this.car_id = car_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.repair_description = repair_description;
+        this.repair_price = repair_price;
     }
 
-    public int getOrderId() {
-        return orderId;
+
+
+    public int getOrder_id() {
+        return order_id;
     }
 
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
+    public void setOrder_id(int order_id) {
+        this.order_id = order_id;
     }
 
-    public int getCarId() {
-        return carId;
+    public int getCar_id() {
+        return car_id;
     }
 
-    public void setCarId(int carId) {
-        this.carId = carId;
+    public void setCar_id(int car_id) {
+        this.car_id = car_id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getFirst_name() {
+        return first_name;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
     }
 
-    public String getLastName() {
-        return lastName;
+    public String getLast_name() {
+        return last_name;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
     }
 
-    public String getRepairDescription() {
-        return repairDescription;
+    public String getRepair_description() {
+        return repair_description;
     }
 
-    public void setRepairDescription(String repairDescription) {
-        this.repairDescription = repairDescription;
+    public void setRepair_description(String repair_description) {
+        this.repair_description = repair_description;
     }
 
-    public double getRepairPrice() {
-        return repairPrice;
+    public double getRepair_price() {
+        return repair_price;
     }
 
-    public void setRepairPrice(double repairPrice) {
-        this.repairPrice = repairPrice;
+    public void setRepair_price(double repair_price) {
+        this.repair_price = repair_price;
     }
+
+
+
 }
--- src/main/java/com/bilabonnementjespers/repositorys/ReturnTestRepo.java
+++ src/main/java/com/bilabonnementjespers/repositorys/ReturnTestRepo.java
@@ -1,4 +1,13 @@
 package com.bilabonnementjespers.repositorys;
-
+@org.springframework.stereotype.Repository
 public class ReturnTestRepo {
+@org.springframework.beans.factory.annotation.Autowired
+    org.springframework.jdbc.core.JdbcTemplate jdbcTemplate;
+
+    public java.util.List<com.bilabonnementjespers.models.ReturnTestModel> testModelList(){
+        String sql = "";
+        javax.swing.tree.RowMapper<com.bilabonnementjespers.models.ReturnTestModel> rowMapper =
+                new org.springframework.jdbc.core.BeanPropertyRowMapper<>(com.bilabonnementjespers.models.ReturnTestModel.class);
+        return jdbcTemplate.query(sql,rowMapper);
+    }
 }
